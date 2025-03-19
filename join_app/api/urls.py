1973a96ba0eb768c3621364a2bb89b829fb39e99
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-03-19 14:45:32 +0100

    First commit

---
+++ join_app/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# from .views import 
+
+urlpatterns = [
+    # path("manufacturers/", ManufacturerList.as_view(), name="manufacturer-list"),
+    # path(
+    #     "manufacturers/<int:pk>/",
+    #     ManufacturerDetail.as_view(),
+    #     name="manufacturer-detail",
+    # ),
+]
