8400d2233fa36e4b9dceec1ce50edb4744d85491
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-03-25 16:45:01 +0100

    make settings for fix cors error

--- join_hub/settings.py
+++ join_hub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
     "rest_framework",
     "rest_framework.authtoken",
     "user_auth_app",
@@ -126,6 +127,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
