13758373426ab2cf80a58614204c4cd215ddddf1
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-04-09 10:20:36 +0200

    add .env and .env_tempate for infos how set up env and where to put key debug and cors orgins

--- join_hub/settings.py
+++ join_hub/settings.py
@@ -11,19 +11,25 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y1w862u$4!z_i#umi-pe8@n1dg!kx!ao$dw(ik&ccv+eps36gk"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -129,7 +135,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",
+    env('CORS_ALLOWED_ORIGINS'),
 ]
 
 REST_FRAMEWORK = {
